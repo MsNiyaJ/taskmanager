74e7234b644a90ce3cad009035541e28000ee8a7
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-06-01 00:44:15 -0400

    added actions

---
+++ src/actions/index.ts
@@ -0,0 +1,25 @@
+// Tasks
+export const addTask = (task: string) => ({
+  type: 'ADD_TASK',
+  payload: task,
+});
+
+export const removeTask = (id: number) => ({
+  type: 'REMOVE_TASK',
+  payload: id,
+});
+
+export const editTask = (id: number, task: string) => ({
+  type: 'EDIT_TASK',
+  payload: { id, task },
+});
+
+export const clearTasks = () => ({
+  type: 'CLEAR_TASKS',
+});
+
+// Display Mode
+export const setDisplayMode = (mode: string) => ({
+  type: 'SET_DISPLAY_MODE',
+  payload: mode,
+});
