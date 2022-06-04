bc2711f41af0461cb375cbd3853010aa2e34070e
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-06-04 01:15:22 -0400

    set theme for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '.';
 
 function App() {
+  const display = useSelector((state: RootState) => state.display);
+
+  const styles = {
+    theme: {
+      backgroundColor: display === 'dark' ? '#000' : '#fff',
+      color: display === 'dark' ? '#fff' : '#000',
+      width: '100vw',
+      height: '100vh',
+    },
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={styles.theme}>
       <h1>Hello World!</h1>
     </div>
   );
